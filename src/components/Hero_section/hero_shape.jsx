2f4e3fc360c13f4fb6843eac95decb01ad4cf462
subhro <subhro.das546@gmail.com>
2025-03-25 00:28:28 +0530

    homepage basic layout

---
+++ src/components/Hero_section/hero_shape.jsx
@@ -0,0 +1,314 @@
+import React from "react";
+
+function Shape() {
+    return (
+      <>
+        <svg
+          width={650}
+          height={650}
+          viewBox="0 0 821 863"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <foreignObject x={-16} y={-18} width={783} height={854}>
+            <div
+              xmlns="http://www.w3.org/1999/xhtml"
+              style={{
+                backdropFilter: "blur(20px)",
+                clipPath: "url(#bgblur_0_1206_31_clip_path)",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </foreignObject>
+          <g filter="url(#filter0_d_1206_31)" data-figma-bg-blur-radius={40}>
+            <path
+              d="M727 384.5C727 558.47 456.311 792 302.5 792C148.689 792 24 558.47 24 384.5C24 210.53 148.689 22 302.5 22C456.311 22 727 210.53 727 384.5Z"
+              fill="url(#pattern0_1206_31)"
+              shapeRendering="crispEdges"
+            />
+          </g>
+          <foreignObject x={-16} y={-18} width={783} height={854}>
+            <div
+              xmlns="http://www.w3.org/1999/xhtml"
+              style={{
+                backdropFilter: "blur(20px)",
+                clipPath: "url(#bgblur_1_1206_31_clip_path)",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </foreignObject>
+          <g filter="url(#filter1_d_1206_31)" data-figma-bg-blur-radius={40}>
+            <path
+              d="M727 384.5C727 558.47 456.311 792 302.5 792C148.689 792 24 558.47 24 384.5C24 210.53 148.689 22 302.5 22C456.311 22 727 210.53 727 384.5Z"
+              fill="url(#paint0_linear_1206_31)"
+              fillOpacity="0.71"
+              shapeRendering="crispEdges"
+            />
+          </g>
+          <foreignObject x={37} y={-36} width={781} height={899}>
+            <div
+              xmlns="http://www.w3.org/1999/xhtml"
+              style={{
+                backdropFilter: "blur(20px)",
+                clipPath: "url(#bgblur_2_1206_31_clip_path)",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </foreignObject>
+          <g filter="url(#filter2_d_1206_31)" data-figma-bg-blur-radius={40}>
+            <path
+              d="M778 387.685C778 571.822 508.081 819 354.708 819C201.334 819 77 571.822 77 387.685C77 203.548 201.334 4 354.708 4C508.081 4 778 203.548 778 387.685Z"
+              fill="url(#pattern1_1206_31)"
+              shapeRendering="crispEdges"
+            />
+          </g>
+          <foreignObject x={37} y={-36} width={800} height={872}>
+            <div
+              xmlns="http://www.w3.org/1999/xhtml"
+              style={{
+                backdropFilter: "blur(20px)",
+                clipPath: "url(#bgblur_3_1206_31_clip_path)",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </foreignObject>
+          <g filter="url(#filter3_d_1206_31)" data-figma-bg-blur-radius={40}>
+            <path
+              d="M797 374.974C797 553.01 519.766 792 362.235 792C204.704 792 77 553.01 77 374.974C77 196.938 204.704 4 362.235 4C519.766 4 797 196.938 797 374.974Z"
+              fill="url(#paint1_linear_1206_31)"
+              fillOpacity="0.37"
+              shapeRendering="crispEdges"
+            />
+          </g>
+          <defs>
+            <filter
+              id="filter0_d_1206_31"
+              x={-16}
+              y={-18}
+              width={783}
+              height={854}
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity={0} result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy={20} />
+              <feGaussianBlur stdDeviation={12} />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1206_31"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1206_31"
+                result="shape"
+              />
+            </filter>
+            <clipPath
+              id="bgblur_0_1206_31_clip_path"
+              transform="translate(16 18)"
+            >
+              <path d="M727 384.5C727 558.47 456.311 792 302.5 792C148.689 792 24 558.47 24 384.5C24 210.53 148.689 22 302.5 22C456.311 22 727 210.53 727 384.5Z" />
+            </clipPath>
+            <pattern
+              id="pattern0_1206_31"
+              patternContentUnits="objectBoundingBox"
+              width="0.3"
+              height="0.3"
+            >
+              <use
+                xlinkHref="#image0_1206_31"
+                transform="scale(0.000426743 0.00038961)"
+              />
+            </pattern>
+            <filter
+              id="filter1_d_1206_31"
+              x={-16}
+              y={-18}
+              width={783}
+              height={854}
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity={0} result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy={20} />
+              <feGaussianBlur stdDeviation={12} />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1206_31"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1206_31"
+                result="shape"
+              />
+            </filter>
+            <clipPath
+              id="bgblur_1_1206_31_clip_path"
+              transform="translate(16 18)"
+            >
+              <path d="M727 384.5C727 558.47 456.311 792 302.5 792C148.689 792 24 558.47 24 384.5C24 210.53 148.689 22 302.5 22C456.311 22 727 210.53 727 384.5Z" />
+            </clipPath>
+            <filter
+              id="filter2_d_1206_31"
+              x={37}
+              y={-36}
+              width={781}
+              height={899}
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity={0} result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy={20} />
+              <feGaussianBlur stdDeviation={12} />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1206_31"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1206_31"
+                result="shape"
+              />
+            </filter>
+            <clipPath
+              id="bgblur_2_1206_31_clip_path"
+              transform="translate(-37 36)"
+            >
+              <path d="M778 387.685C778 571.822 508.081 819 354.708 819C201.334 819 77 571.822 77 387.685C77 203.548 201.334 4 354.708 4C508.081 4 778 203.548 778 387.685Z" />
+            </clipPath>
+            <pattern
+              id="pattern1_1206_31"
+              patternContentUnits="objectBoundingBox"
+              width="0.300856"
+              height="0.283436"
+            >
+              <use
+                xlinkHref="#image0_1206_31"
+                transform="scale(0.00042796 0.000368098)"
+              />
+            </pattern>
+            <filter
+              id="filter3_d_1206_31"
+              x={37}
+              y={-36}
+              width={800}
+              height={872}
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity={0} result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                result="hardAlpha"
+              />
+              <feOffset dy={20} />
+              <feGaussianBlur stdDeviation={12} />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow_1206_31"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow_1206_31"
+                result="shape"
+              />
+            </filter>
+            <clipPath
+              id="bgblur_3_1206_31_clip_path"
+              transform="translate(-37 36)"
+            >
+              <path d="M797 374.974C797 553.01 519.766 792 362.235 792C204.704 792 77 553.01 77 374.974C77 196.938 204.704 4 362.235 4C519.766 4 797 196.938 797 374.974Z" />
+            </clipPath>
+            <linearGradient
+              id="paint0_linear_1206_31"
+              x1="375.5"
+              y1={22}
+              x2="375.5"
+              y2={792}
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#00950F" />
+              <stop offset={1} stopColor="#213D29" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear_1206_31"
+              x1={437}
+              y1={4}
+              x2={437}
+              y2={792}
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#59E6FF" />
+              <stop offset={1} stopColor="#001E24" />
+            </linearGradient>
+          </defs>
+          <text
+            x="300"
+            y="400"
+            fontSize="40"
+            fill="white"
+            textAnchor="middle"
+            fontFamily="Arial, sans-serif"
+          >
+            We 
+            Care
+          </text>
+        </svg>
+      </>
+    );
+  }
+  
+  export default Shape
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Shape from '../../components/Hero_section/hero_shape'
+import Box from '../../components/Hero_section/glasspane'
+import Navbar from '../../components/navbar/navbar'
+import './Homepage.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-[#242424]'>
+      <Navbar/>
+      <div className="main_container">
+        <Shape/>
+        <div className="box_container">
+          <Box/>
+          <Box/>
+        </div>
+      </div>
+    </div>
   )
 }
 
