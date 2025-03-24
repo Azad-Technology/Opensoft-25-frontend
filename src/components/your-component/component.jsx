37fec6df1cd694bad16df75d1a3e1e1475328b96
Harsh Dalmia <harshdalmia20212081@gmail.com>
2025-03-24 23:32:41 +0530

    Folder structure

--- src/components/your-component/component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Component = () => {
-  return <div class="text-center text-3xl">Component</div>;
-};
-
-export default Component;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/app/app";
+import App from "./App";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+   <App/>
   </StrictMode>,
 );
