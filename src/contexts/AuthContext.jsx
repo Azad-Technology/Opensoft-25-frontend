0bf86fe4922a8bf19be6bd821a78bb8f17581c21
Anuragp22 <anuragp2003b@gmail.com>
2025-04-04 02:20:18 +0530

    removing lastSessionId on logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -70,7 +70,6 @@ export const AuthProvider = ({ children }) => {
     }
   }, [user, isLoading, location.pathname, navigate]);
 
-
   const login = async (userData, access_token) => {
     setIsLoading(true);
     try {
@@ -89,39 +88,41 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     setUser(null);
     localStorage.removeItem("auth");
+    localStorage.removeItem("lastSessionId");
     navigate("/login");
   };
-
 
   const refreshToken = async () => {
     const tokenData = JSON.parse(localStorage.getItem("auth"));
     try {
-      const response = await fetch(`${import.meta.env.VITE_REACT_APP_URL}/auth/token`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${tokenData.access_token}`,
-        }
-      })
+      const response = await fetch(
+        `${import.meta.env.VITE_REACT_APP_URL}/auth/token`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${tokenData.access_token}`,
+          },
+        },
+      );
 
-      const res= await response.json();
-      const expTime=Date.now()+(res.expires_in*1000);
+      const res = await response.json();
+      const expTime = Date.now() + res.expires_in * 1000;
 
-      const newTokenData={
-        access_token:res.access_token,
-        expiration:expTime,
-        user:tokenData.user
-      }
+      const newTokenData = {
+        access_token: res.access_token,
+        expiration: expTime,
+        user: tokenData.user,
+      };
 
       // localStorage.removeItem("auth");
-      localStorage.setItem("auth",JSON.stringify(newTokenData));
+      localStorage.setItem("auth", JSON.stringify(newTokenData));
       setToken(newTokenData.access_token);
     } catch (error) {
       console.error("refresh token error:", error);
       toast.error("Error refreshing token");
     }
-
-  }
+  };
 
   return (
     <AuthContext.Provider
@@ -133,7 +134,7 @@ export const AuthProvider = ({ children }) => {
         login,
         logout,
         setUser,
-        refreshToken
+        refreshToken,
       }}
     >
       {children}
